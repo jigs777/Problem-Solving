3c4a1bc3db4593229c9db43bc3ceccbf0f08d82e
jigar parmar <parmarjigar555@gmail.com>
2022-03-16 02:06:48 +0530

    From top to down

---
+++ Logical Thinking 2/From top to down.swift
@@ -0,0 +1,55 @@
+// Q4. From top to down
+
+// Problem Description
+/*
+ Write a program to print all Natural numbers from 1 to N where you have to take N as input from user
+ */
+
+/*Problem Constraints
+ 1 <= N <= 10000000
+ */
+
+//Input Format
+//A single line representing N
+
+//Output Format
+//N space separated integers from 1 to N.
+
+//YOUR CODE GOES HERE
+//Swift
+
+let n = Int(readLine()!)
+var a = 0
+while(a < n!){
+    a = a + 1
+    print( a,"", separator: " ", terminator:"")
+}
+
+//Example Input
+//Input 1:
+//5
+
+//Input 2:
+//10
+
+////Example Output
+//Output 1:
+//1 2 3 4 5
+
+//Output 2:
+//1 2 3 4 5 6 7 8 9 10
+
+
+//Example Explanation
+
+//Explanation 1:
+// For Student 1, percentage = (50+60+70+80+90)/5 = 70. Hence grade is C.
+
+//Explanation 2:
+// For Student 2, percentage = (90+90+90+90+90)/5 = 90. Hence grade is A.
+
+
+
+
+
+
