c45eb4deef267cac0707e557c391309280b596d6
jigar parmar <parmarjigar555@gmail.com>
2022-03-16 23:39:48 +0530

    Binary to Decimal - II

---
+++ Functions/Binary to Decimal - II.swift
@@ -0,0 +1,69 @@
+// Q1. Binary to Decimal - II
+
+// Problem Description
+/*
+ You are given a number A in binary format (Base = 2). You have to print the number in decimal format (Base = 10).
+ */
+
+/*Problem Constraints
+ 1 <= A <= 210
+ */
+
+//Input Format
+//First and only line contains a single binary number A.
+
+//Output Format
+//Print in a single line, a decimal integer.
+
+//YOUR CODE GOES HERE
+//Swift
+
+var n = Int(readLine()!)
+var ans = 0
+var power = 1
+while(n! > 0){
+    let lastdig = n! % 10
+    ans = ans + lastdig * power
+    n! = n! / 10
+    power = 2 * power
+}
+print(ans)
+
+//Example Input
+//Input 1:
+//11
+
+//Input 2:
+//1011
+
+////Example Output
+//Output 1:
+// 3
+
+//Output 2:
+//11
+
+//Explanation 1:
+// (0011)2 = (21 + 20)10 = (3)10
+
+//Explanation 2:
+// (1010)2 = (23+ 21 + 20)10 = (11)10
+
+
+
+
+
+
+//Example Explanation
+
+//Explanation 1:
+// For Student 1, percentage = (50+60+70+80+90)/5 = 70. Hence grade is C.
+
+//Explanation 2:
+// For Student 2, percentage = (90+90+90+90+90)/5 = 90. Hence grade is A.
+
+
+
+
+
+
